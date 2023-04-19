aebf5420ff0e56cb791d836975dbfa91655a3d0a
GlebTanaka <69252964+GlebTanaka@users.noreply.github.com>
2023-04-19 08:46:34 +0200

    add environment for development
    register EmployeeService as provider in module file
    
    create base url for service

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  private apiServiceUrl = '';
+  private apiServiceUrl = environment.apiBaseUrl;
   constructor(private http: HttpClient) {}
 
   public getEmployees(): Observable<Employee[]> {
